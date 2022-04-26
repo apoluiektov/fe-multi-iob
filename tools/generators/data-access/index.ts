0cfae84768b1c9c18d9551f15fdb21ae41350c55
alexpol001 <alexpol001@yandex.ru>
2022-04-26 12:24:25 +0300

    bugs: fix data-access, ui generators

--- tools/generators/data-access/index.ts
+++ tools/generators/data-access/index.ts
@@ -3,12 +3,12 @@ import { Tree, formatFiles, installPackagesTask } from '@nrwl/devkit';
 import { LibrarySchema, LibraryType } from '../../utils';
 import { generateTypedLibrary } from '../../utils/generate-typed-library';
 
-export interface UiSchema extends LibrarySchema {}
+export interface DataAccessSchema extends LibrarySchema {}
 
-export default async function (tree: Tree, schema: UiSchema) {
+export default async function (tree: Tree, schema: DataAccessSchema) {
   await generateTypedLibrary(tree, {
     ...schema,
-    type: LibraryType.UI,
+    type: LibraryType.DATA_ACCESS,
   });
   await formatFiles(tree);
   return () => {
--- tools/generators/ui/index.ts
+++ tools/generators/ui/index.ts
@@ -1,8 +1,15 @@
 import { Tree, formatFiles, installPackagesTask } from '@nrwl/devkit';
-import { libraryGenerator } from '@nrwl/workspace/generators';
 
-export default async function (tree: Tree, schema: any) {
-  await libraryGenerator(tree, { name: schema.name });
+import { LibrarySchema, LibraryType } from '../../utils';
+import { generateTypedLibrary } from '../../utils/generate-typed-library';
+
+export interface UiSchema extends LibrarySchema {}
+
+export default async function (tree: Tree, schema: UiSchema) {
+  await generateTypedLibrary(tree, {
+    ...schema,
+    type: LibraryType.UI,
+  });
   await formatFiles(tree);
   return () => {
     installPackagesTask(tree);
